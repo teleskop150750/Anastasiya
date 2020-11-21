1b87f5a4f0fa7931876c43781067c9b930c18996
teleskop150750 <teleskop150750@gmail.com>
2020-11-21 22:26:46 +0300

    3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-new Photostack(document.querySelector('#photostack-1'), {
+/* eslint-disable no-new */
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+new Photostack(document.getElementById('photostack-3'), {
   showNavigation: false,
-  afterShowPhoto(ps) {
-    setTimeout(() => {
+  afterShowPhoto: function (ps) {
+    setTimeout(function () {
       ps._navigate('next');
     }, 3500);
-  },
+  }
 });
