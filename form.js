ba687de9738133592486bcd1f130e6d493af0122
teleskop150750 <teleskop150750@gmail.com>
2020-11-22 14:48:36 +0300

    1

---
+++ form.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll('.form__input');
+inputs.forEach((input) => {
+  input.addEventListener('focus', () => {
+    const parent = input.closest('.form__group');
+    parent.classList.add('form__group--focus', 'form__group--value');
+  });
+  input.addEventListener('blur', function f() {
+    const parent = input.closest('.form__group');
+    if (this.value.trim() === '') {
+      parent.classList.remove('form__group--value');
+      this.value = '';
+    }
+    parent.classList.remove('form__group--focus');
+  });
+});
