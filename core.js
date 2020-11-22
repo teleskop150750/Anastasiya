60cc6467a2e8b8b3befc14f4b193fc0f147468de
teleskop150750 <teleskop150750@gmail.com>
2020-11-22 11:02:08 +0300

    !

--- core.js
+++ core.js
@@ -1,36 +1,33 @@
 // navToggle
 
 const nav = document.querySelector('.nav__list');
-const navToggle = (e) => {
-  const { target } = e;
-  target.removeEventListener('click', navToggle);
+const navToggle = function f() {
+  this.removeEventListener('click', navToggle);
 
   if (!nav.classList.contains('nav__list--open')) {
-    nav.classList.add('nav__list--opening');
     nav.style.height = `${nav.scrollHeight}px`;
 
-    target.classList.remove('nav-button--open');
-    target.classList.add('nav-button--close');
+    this.classList.remove('nav-button--open');
+    this.classList.add('nav-button--close');
 
     const transitionShow = () => {
       nav.classList.add('nav__list--open');
-      nav.classList.remove('nav__list--opening');
       nav.removeEventListener('transitionend', transitionShow);
-      target.addEventListener('click', navToggle);
+      this.addEventListener('click', navToggle);
     };
 
     nav.addEventListener('transitionend', transitionShow);
   } else {
     nav.style.height = '0px';
 
-    target.classList.remove('nav-button--close');
-    target.classList.add('nav-button--open');
+    this.classList.remove('nav-button--close');
+    this.classList.add('nav-button--open');
 
     const transitionHidden = () => {
       nav.removeAttribute('style');
       nav.classList.remove('nav__list--open');
       nav.removeEventListener('transitionend', transitionHidden);
-      target.addEventListener('click', navToggle);
+      this.addEventListener('click', navToggle);
     };
     nav.addEventListener('transitionend', transitionHidden);
   }
