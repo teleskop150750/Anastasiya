71b818a206689b4563eeae80ba5728bf887d42bf
teleskop150750 <teleskop150750@gmail.com>
2020-11-24 00:38:44 +0300

    1

--- core.js
+++ core.js
@@ -1,40 +1,20 @@
+import SlideToggle from './scripts/slideToggle.js';
 // navToggle
-
 const nav = document.querySelector('.nav__list');
-const navToggle = function f() {
-  this.removeEventListener('click', navToggle);
-
-  if (!nav.classList.contains('nav__list--open')) {
-    nav.classList.add('nav__list--opening');
-    nav.style.height = `${nav.scrollHeight}px`;
-
-    this.classList.remove('nav-button--open');
-    this.classList.add('nav-button--close');
-    this.setAttribute('aria-label', 'закрыть меню');
-
-    const transitionShow = () => {
-      nav.classList.remove('nav__list--opening');
-      nav.classList.add('nav__list--open');
-      nav.removeEventListener('transitionend', transitionShow);
-      this.addEventListener('click', navToggle);
-    };
-
-    nav.addEventListener('transitionend', transitionShow);
-  } else {
-    nav.style.height = '0px';
-
-    this.classList.remove('nav-button--close');
-    this.classList.add('nav-button--open');
-    this.setAttribute('aria-label', 'открыть меню');
-
-    const transitionHidden = () => {
-      nav.removeAttribute('style');
-      nav.classList.remove('nav__list--open');
-      nav.removeEventListener('transitionend', transitionHidden);
-      this.addEventListener('click', navToggle);
-    };
-    nav.addEventListener('transitionend', transitionHidden);
-  }
-};
 const navButton = document.querySelector('.nav-button');
-navButton.addEventListener('click', navToggle, false);
+
+// eslint-disable-next-line no-new
+new SlideToggle({
+  target: nav,
+  button: navButton,
+  opening: 'nav__list--opening',
+  open: 'nav__list--open',
+  beforeSlideDown() {
+    this.button.classList.remove('nav-button--open');
+    this.button.classList.add('nav-button--close');
+  },
+  beforeSlideUp() {
+    this.button.classList.add('nav-button--open');
+    this.button.classList.remove('nav-button--close');
+  },
+});
