6f1a884fc8e2bf4269013bf5c9e534349dbb62f8
teleskop150750 <teleskop150750@gmail.com>
2020-11-27 22:06:55 +0300

    1

--- scripts/formSubmit.js
+++ scripts/formSubmit.js
@@ -5,6 +5,7 @@ export default (form, inputs) => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     let isErrors = false;
+
     inputs.forEach((input) => {
       if (input.value.trim() === '') {
         isErrors = true;
@@ -12,10 +13,35 @@ export default (form, inputs) => {
         parent.classList.add('form__group--error');
       }
     });
+
     if (!isErrors) {
       formDefault(inputs);
+      const modal = document.querySelector('.modal');
+      const modalBodu = modal.querySelector('.modal__body');
+      let mmodalBoduValue = '';
+      switch (document.querySelector('.form__input--select').value) {
+        case 'thanks':
+          mmodalBoduValue = `
+            <p class="modal__text">Спасибо за благодарность.</p>
+            <p class="modal__text">Мы рады что вы остались довольны!</p>
+          `;
+          break;
+        case 'sentence':
+          mmodalBoduValue = `
+            <p class="modal__text">Мы обязательно в ближайшее время рассмотрим ваше пожелание.</p>
+          `;
+          break;
+        case 'complaint':
+          mmodalBoduValue = `
+            <p class="modal__text">Ваша жалоба будет рассмотренна в ближайщее время.</p>
+            <p class="modal__text">Приносим свои извинения.</p>
+          `;
+          break;
+        default:
+          break;
+      }
+      modalBodu.innerHTML = mmodalBoduValue;
+      modalOpen(modal);
     }
-    const modal = document.querySelector('.modal');
-    modalOpen(modal);
   });
 };
